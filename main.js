82652cf4ec7d01ac864dcb01f236360dc4b561bf
User <issaadibtissem101@gmail.com>
2024-12-05 19:27:59 -0800

    modif video

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const menuIcon = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
 const closeIcon = document.querySelector(".close");
-menuIcon.addEventListener("click", () => {
-  navbar.classList.add("active");
-});
-closeIcon.addEventListener("click", () => {
-  navbar.classList.remove("active");
-});
+
+if (menuIcon) {
+  menuIcon.addEventListener("click", () => {
+    navbar.classList.add("active");
+  });
+}
+if (closeIcon) {
+  closeIcon.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+}
