170d12840b541fc2da2f524713c4c6a61478ec37
User <issaadibtissem101@gmail.com>
2024-12-06 21:58:13 -0800

    added custom cursor

--- main.js
+++ main.js
@@ -12,3 +12,17 @@ if (closeIcon) {
     navbar.classList.remove("active");
   });
 }
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  innerCursor.style.left = `${x}px`;
+  innerCursor.style.top = `${y}px`;
+  outerCursor.style.left = `${x}px`;
+  outerCursor.style.top = `${y}px`;
+}
